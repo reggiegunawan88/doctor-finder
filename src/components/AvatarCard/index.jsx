8e7af4e3cbb1a36bf133c13d4e1648b65162e98f
reggiegunawan <reggiegunawan88@gmail.com>
2022-01-27 13:41:13 +0700

    feat(avatar-card): add about description

--- src/components/AvatarCard/index.jsx
+++ src/components/AvatarCard/index.jsx
@@ -1,5 +1,8 @@
+import { clearHTMLTag } from "utils/helper";
+
 function AvatarCard(props) {
   const doctor = props.data;
+
   return (
     <div className="border-default border-black p-3 rounded-lg">
       <div className="grid grid-cols-2 gap-x-3">
@@ -8,12 +11,21 @@ function AvatarCard(props) {
         <div className="flex flex-col gap-y-5">
           {/* doctor description */}
           <div className="flex flex-col flex-grow gap-y-2 mx-4">
-            <p>Name: {doctor.name}</p>
-            <p>Hospital: {doctor.hospital[0].name}</p>
-            <p>Specialization: {doctor.specialization.name}</p>
+            <p>
+              <b>Name:</b> {doctor.name}
+            </p>
+            <p>
+              <b>Hospital:</b> {doctor.hospital[0].name}
+            </p>
+            <p>
+              <b>Specialization:</b> {doctor.specialization.name}
+            </p>
+            <p className=" break-words">
+              <b>About:</b> {clearHTMLTag(doctor.about)}
+            </p>
           </div>
           {/* price */}
-          <p className="text-right">Price: Rp {doctor.price.raw}</p>
+          <p className="text-right font-bold text-lg">Price: Rp {doctor.price.raw}</p>
         </div>
       </div>
     </div>
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,10 @@
 // helper for reusable functions
+
+export const clearHTMLTag = (str) => {
+  // Create a temp div element
+  var tempDivElement = document.createElement("div");
+  // Set the HTML content with param value
+  tempDivElement.innerHTML = str;
+  // Return the text property of the element
+  return tempDivElement.textContent || tempDivElement.innerText || "";
+};
